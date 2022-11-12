db50133700a198f8020f37b2c6e047189c74de65
Wonil <red112@naver.com>
2022-11-12 13:16:17 +0900

    BananaMan으로 케릭터 변경

---
+++ RaceOnLink/Assets/Scripts/TPSCameraController2.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPSCameraController2 : MonoBehaviour
+{
+    [SerializeField]
+    private Transform characterBody;
+    [SerializeField]
+    private Transform cameraArm;
+
+
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = characterBody.GetComponent<Animator>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAround(); 
+        Move();       
+    }
+
+    private void Move()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isMove = moveInput.magnitude != 0;
+        animator.SetBool("IsRun", isMove);
+        if(isMove)
+        {
+            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            characterBody.forward = moveDir;
+            transform.position += moveDir * Time.deltaTime * 5f;
+        }
+    }
+
+    private void LookAround()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * 3f;
+        Vector3 camAngle = cameraArm.rotation.eulerAngles;
+
+        float x = camAngle.x - mouseDelta.y;
+        if(x<10f) {
+            x = 10f;
+        }
+        else if(x>50f)
+        {
+            x = 50f;
+        }
+
+        cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
+    }
+}
